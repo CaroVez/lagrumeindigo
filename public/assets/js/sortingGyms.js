ab7c046026b2022654e5343e71788ddd2b0103a8
Caroline Vézina <caroline_vezina@hotmail.com>
2022-11-17 11:38:26 +0100

    updating javascript sorting file

--- public/assets/js/sortingGyms.js
+++ public/assets/js/sortingGyms.js
@@ -3,9 +3,9 @@ const all = document.querySelector('.all');
 const activated = document.querySelector('.activated');
 const disabled = document.querySelector('.desactives');
 
-// ########### SORTING ########### //
+// ########### SORTING GYM ########### //
 function sortingItems(status) {
-  const items = document.querySelectorAll(".accordion-item");
+  const items = document.querySelectorAll(".accordion-item-gym");
   for (let i = 0; i < items.length; i++) {       
     if (items[i].className != status) {
       items[i].hidden = true;
@@ -17,24 +17,24 @@ function sortingItems(status) {
 
 // ########### EVENT ON CLICK ########### //
 all.addEventListener('click', () => {
-  const items = document.querySelectorAll(".accordion-item");
+  const items = document.querySelectorAll(".accordion-item-gym");
   for (let i = 0; i < items.length; i++) {    
     items[i].hidden = false;
   }  
 });
 
 activated.addEventListener('click', () => {
-  sortingItems('accordion-item activated');  
+  sortingItems('accordion-item-gym activated');  
 });
 
 disabled.addEventListener('click', () => {
-  sortingItems('accordion-item disabled');
+  sortingItems('accordion-item-gym disabled');
 });
 
 // ########### SEARCH BAR ########### //
 function search() {
   const input = document.getElementById('search').value.toLowerCase();
-  const items = document.querySelectorAll(".accordion-item");  
+  const items = document.querySelectorAll(".accordion-item-gym");  
   for (i = 0; i < items.length; i++) {
     if (!items[i].innerText.toLowerCase().includes(input)) {
       items[i].hidden = true;
