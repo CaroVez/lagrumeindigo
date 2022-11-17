0853bf73b33ee2d5425de2c55fb3a1058d2efddd
Caroline Vézina <caroline_vezina@hotmail.com>
2022-11-17 11:47:26 +0100

    online deployment

--- public/index.php
+++ public/index.php
@@ -7,3 +7,31 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+// database
+// heroku JawsDB
+$url = getenv('JAWSDB_URL');
+if ($url !== false) {
+    $dbparts = parse_url($url);
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+} else {
+//local
+    $username = 'root';
+    $password = '';
+    $database = 'lagrumeindigo';
+    $hostname = 'localhost';
+};
+
+try {
+    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
